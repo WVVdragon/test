fa973f01a408db85e34d52b7fa8dfa87f9bd5f06
WEi <925168472@qq,com>
2020-12-06 04:32:44 +0800

    new

--- parent.cpp
+++ parent.cpp
@@ -1,36 +1,44 @@
-#include<unistd.h>
-#include<stdio.h> 
-#include<time.h>
-#include<windows.h>
-#include<stdlib.h>
-#include<conio.h> 
-void child(void)
-{
-	int i;
-	for(i = 1;i < 1666;i++)
-	{
-		time_t timep;
-		struct tm *p;
-		time (&timep);
-		p=gmtime(&timep);
-		printf("The child is talking at%d:%d:%d\n",8+p->tm_hour,p->tm_min,p->tm_sec);
-		sleep(1);
-	}
-}
+#include <unistd.h>
+#include <stdio.h>
+
 int main(void)
 {
     pid_t pid;
+
+    int n = 0;  //测试父进程和子进程是否共享一个n
+
     pid = fork();
-    child();
-    int i;
-	for(i = 1;i < 1666;i++)
-	{
-		time_t timep;
-		struct tm *p;
-		time (&timep);
-		p=gmtime(&timep);
-		printf("The parent is talking at%d:%d:%d\n",8+p->tm_hour,p->tm_min,p->tm_sec);
-		sleep(1);
-	}
-	
+
+    if(pid < 0)
+    {
+        /* 创建子进程失败 */
+        printf("fork fail.\n");
+        return -1;
+    }
+    else if(pid == 0)
+    {
+       	int i;
+		for(i = 1;i < 1666;i++)
+		{
+			time_t timep;
+			struct tm *p;
+			time (&timep);
+			p=gmtime(&timep);
+			printf("The child is talking at%d:%d:%d\n",8+p->tm_hour,p->tm_min,p->tm_sec);
+			sleep(1);
+		}
+    }
+    else
+    {
+       	int i;
+		for(i = 1;i < 1666;i++)
+		{
+			time_t timep;
+			struct tm *p;
+			time (&timep);
+			p=gmtime(&timep);
+			printf("The parent is talking at%d:%d:%d\n",8+p->tm_hour,p->tm_min,p->tm_sec);
+			sleep(1);
+		}
+    }
 }
