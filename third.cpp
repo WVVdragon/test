7258fdedb36c16f0b76f5a3d3efd23a2c85dae26
WEi <925168472@qq,com>
2020-12-06 14:47:32 +0800

    new

--- third.cpp
+++ third.cpp
@@ -1,17 +1,19 @@
-#include "stdio.h"  
-#include "pthread.h"  
-#include "stdlib.h"  
-pthread_t thread1;  
-  
-void *thread_dec(void *arg){  
-   printf("child%d\n",pthread_self());
-}  
-void main(void *arg){  
-       
-     int i = pthread_create(&thread1,NULL,thread_dec,NULL);   //创建子线程  
-     if(i != 0){  
-         printf("线程创建失败！\n");  
-     }  
-     printf("parent%d\n",pthread_self());   //调用自增1函数  
-     return ;  
-}  
+#include <pthread.h>
+#include <stdio.h>
+ 
+void* thread_func(void *arg)
+{
+    printf("thread id=%lu\n", pthread_self());
+    return arg;
+}
+ 
+int main(void)
+{
+    pid_t pid;
+    pthread_t tid;
+    pid = getpid();
+    printf("process id=%d\n", pid);
+    pthread_create(&tid, NULL, thread_func, NULL);
+    pthread_join(tid,NULL);
+    return 0;
+}
