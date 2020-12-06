b9e96f3073f0985455d62c5f03e9382f7309c9da
WEi <925168472@qq,com>
2020-12-06 15:37:45 +0800

    new

--- third.cpp
+++ third.cpp
@@ -1,16 +1,18 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
-void* thread_func(void *arg)
+void pTime()
 {
-    while(1)
-    {
-        time_t timep;
+	time_t timep;
 	struct tm *p;
 	time (&timep);
 	p=gmtime(&timep);
-	printf("Child thread is talking at%d:%d:%d | thread id=%lu\n",8+p->tm_hour,p->tm_min,p->tm_sec,pthread_self());
-    }
+	printf("Child thread is talking at%d:%d:%d",8+p->tm_hour,p->tm_min,p->tm_sec);
+} 
+void* thread_func(void *arg)
+{
+	pTime();
+	printf(" thread id=%lu\n",pthread_self());
     return arg;
 }
  
@@ -19,11 +21,7 @@ int main(void)
     pid_t pid;
     pthread_t tid;
     pid = getpid();
-    time_t timep;
-    struct tm *p;
-    time (&timep);
-    p=gmtime(&timep);
-    printf("Main thread is talking at%d:%d:%d | ",8+p->tm_hour,p->tm_min,p->tm_sec);
+    pTime();
     printf("process id=%d\n", pid);
     pthread_create(&tid, NULL, thread_func, NULL);
     pthread_join(tid,NULL);
